2b4e09886b8fe464e1d7069c74c5f693c6d21fb5
Jadeilton <jadeiltondm@gmail.com>
2024-04-02 17:42:55 -0300

    feat: Usuario create constructor

--- src/main/java/web/dio/api/my/dio/api/doMain/Usuario.java
+++ src/main/java/web/dio/api/my/dio/api/doMain/Usuario.java
@@ -1,4 +1,19 @@
 package web.dio.api.my.dio.api.doMain;
 
 public class Usuario {
+
+    private String login;
+    private String senha;
+
+
+    public Usuario(String senha, String login) {
+
+        this.login = login;
+        this.senha = senha;
+    }
+
+
+
+
+
 }
