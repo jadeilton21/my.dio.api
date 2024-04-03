9549ff2d8a208740cc77e2d7ff6cf04c8e450773
Jadeilton <jadeiltondm@gmail.com>
2024-04-03 10:23:17 -0300

    feat: create method cadastrar in UsuarioControllers

--- src/main/java/web/dio/api/my/dio/api/controllers/UsuarioControllers.java
+++ src/main/java/web/dio/api/my/dio/api/controllers/UsuarioControllers.java
@@ -1,6 +1,7 @@
 package web.dio.api.my.dio.api.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,11 +12,11 @@ import web.dio.api.my.dio.api.doMain.Usuario;
 public class UsuarioControllers {
     @Autowired
     private UsuarioRepository repository;
-
+    @PostMapping
     public void cadastrar(@RequestBody Usuario usuario){
 
 
-
+            repository.save(usuario);
     }
 
 
