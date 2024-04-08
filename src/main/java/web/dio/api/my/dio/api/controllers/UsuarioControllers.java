3a0acdb1b28e96beafa906730b44768c7e06e4e2
Jadeilton <jadeiltondm@gmail.com>
2024-04-08 15:35:25 -0300

    feat: create class UsuarioRepository

--- src/main/java/web/dio/api/my/dio/api/controllers/UsuarioControllers.java
+++ src/main/java/web/dio/api/my/dio/api/controllers/UsuarioControllers.java
@@ -3,6 +3,7 @@ package web.dio.api.my.dio.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import web.dio.api.my.dio.api.doMain.Usuario;
+import web.dio.api.my.dio.api.repository.UsuarioRepository;
 
 @RestController
 @RequestMapping("/usuarios")
