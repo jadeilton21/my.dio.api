977bfa1c9aa66ce20903a87222c91ec2a02c2c3d
Jadeilton <jadeiltondm@gmail.com>
2024-04-08 15:44:26 -0300

    feat: add anotation repository

--- src/main/java/web/dio/api/my/dio/api/repository/UsuarioRepository.java
+++ src/main/java/web/dio/api/my/dio/api/repository/UsuarioRepository.java
@@ -1,4 +1,10 @@
 package web.dio.api.my.dio.api.repository;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
+
+@Repository
 public class UsuarioRepository {
+
+
 }
