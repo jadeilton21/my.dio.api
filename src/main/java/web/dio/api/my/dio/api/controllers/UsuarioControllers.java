408c413243827d81d3242e3e2d542960fe100f17
Jadeilton <jadeiltondm@gmail.com>
2024-04-08 22:28:27 -0300

    feat: update class controller

--- src/main/java/web/dio/api/my/dio/api/controllers/UsuarioControllers.java
+++ src/main/java/web/dio/api/my/dio/api/controllers/UsuarioControllers.java
@@ -5,28 +5,38 @@ import org.springframework.web.bind.annotation.*;
 import web.dio.api.my.dio.api.doMain.Usuario;
 import web.dio.api.my.dio.api.repository.UsuarioRepository;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioControllers {
     @Autowired
     private UsuarioRepository repository;
     @PostMapping()
-    public void cadastrar(@RequestBody Usuario usuario){
+    public void cadastrarUsuario(@RequestBody Usuario usuario){
 
 
             repository.save(usuario);
     }
     @PutMapping()
-    public void atualizar(@RequestBody Usuario usuario){
+    public void atualizarUsuario(@RequestBody Usuario usuario){
         repository.update(usuario);
 
     }
-    @DeleteMapping
-    public void deletar(@RequestBody Usuario usuario){
+    @DeleteMapping({"id"})
+    public void deletarUsuario(@PathVariable("id") Integer id){
 
-        repository.delete(usuario);
+        repository.remove(id);
 
 
+    }
+    @GetMapping
+    public List<Usuario> listarUsuario(){
+        return repository.listAll();
+    }
+    @GetMapping("{id}")
+    public Usuario pegarUm(@PathVariable("id") Integer id){
+        return repository.finById(id);
     }
 
 
