69071907c3ff8d29c75e9a27fb5194fcd44a455a
Jadeilton <jadeiltondm@gmail.com>
2024-04-08 22:59:51 -0300

    feat: create methods in UsuarioRepository

--- src/main/java/web/dio/api/my/dio/api/repository/UsuarioRepository.java
+++ src/main/java/web/dio/api/my/dio/api/repository/UsuarioRepository.java
@@ -2,9 +2,28 @@ package web.dio.api.my.dio.api.repository;
 
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RestController;
+import web.dio.api.my.dio.api.doMain.Usuario;
+
+import java.util.List;
 
 @Repository
 public class UsuarioRepository {
 
 
+    public void save(Usuario usuario) {
+
+
+    }
+
+    public void update(Usuario usuario) {
+    }
+
+    public void remove(Integer id) {
+    }
+
+    public List<Usuario> listAll() {
+    }
+
+    public Usuario finById(Integer id) {
+    }
 }
