17c222ddf9a0944fc14de815ad467848fd4934a4
Jadeilton <jadeiltondm@gmail.com>
2024-04-10 09:02:40 -0300

    feat: add anotation SpringBootTest

--- src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
+++ src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
@@ -1,7 +1,9 @@
 package web.dio.api.my.dio.api.controllers;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
 class UsuarioControllersTest {
 
 }
