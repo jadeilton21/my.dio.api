2f2ad937cb9184cb4f0dcda0db3b5bad93de3c42
Jadeilton <jadeiltondm@gmail.com>
2024-04-10 09:24:35 -0300

    feat: method cadastrarUsuario, tested.

--- src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
+++ src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -21,10 +22,10 @@ class UsuarioControllersTest {
     @DisplayName("Deve devolver Código Http 404 quando informações estiverem invalidas..")
     void cadastrarUsuario() throws Exception {
         var response = mockMvc.perform(
-                post("usuario")
+                post("/usuario")
         ).andReturn().getResponse();
 
         assertThat(response.getStatus())
-                .isEqualTo()
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
