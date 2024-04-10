fb2752128ca2b498c4467bcb55ec0970352912a3
Jadeilton <jadeiltondm@gmail.com>
2024-04-10 09:03:16 -0300

    feat: add anotation AutoConfigureMockMvc

--- src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
+++ src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
@@ -1,9 +1,11 @@
 package web.dio.api.my.dio.api.controllers;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
+@AutoConfigureMockMvc
 class UsuarioControllersTest {
 
 }
