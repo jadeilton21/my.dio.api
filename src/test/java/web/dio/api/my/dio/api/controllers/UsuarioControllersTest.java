44ad0aeca37b2fb48f078b3e24d4746ae8910d3f
Jadeilton <jadeiltondm@gmail.com>
2024-04-10 09:10:09 -0300

    feat: private MockMvc

--- src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
+++ src/test/java/web/dio/api/my/dio/api/controllers/UsuarioControllersTest.java
@@ -1,11 +1,15 @@
 package web.dio.api.my.dio.api.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 @AutoConfigureMockMvc
 class UsuarioControllersTest {
+    @Autowired
+    private MockMvc mockMvc;
 
 }
