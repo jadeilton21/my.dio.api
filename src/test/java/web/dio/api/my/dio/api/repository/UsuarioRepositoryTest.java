ce2ab65db21bf590a3612b14958b10c352d2c204
Jadeilton <jadeiltondm@gmail.com>
2024-04-11 19:15:11 -0300

    feat:  tested method save

--- src/test/java/web/dio/api/my/dio/api/repository/UsuarioRepositoryTest.java
+++ src/test/java/web/dio/api/my/dio/api/repository/UsuarioRepositoryTest.java
@@ -5,9 +5,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.http.RequestEntity.post;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 class UsuarioRepositoryTest {
@@ -19,5 +24,12 @@ class UsuarioRepositoryTest {
     @DisplayName("Deve devolver Código Http 404 quando informações estiverem invalidas..")
     void save() throws Exception{
 
+        var response = mockMvc.perform(
+                (RequestBuilder) post("/usuario")
+        ).andReturn().getResponse();
+
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
