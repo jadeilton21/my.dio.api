64504f995ebe847904f3e08eccf9c9cdb049dae6
Jadeilton <jadeiltondm@gmail.com>
2024-04-14 16:22:27 -0300

    feat: detalheApi class test

--- src/test/java/web/dio/api/my/dio/api/doc/SwaggerConfigTest.java
+++ src/test/java/web/dio/api/my/dio/api/doc/SwaggerConfigTest.java
@@ -5,8 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
@@ -25,5 +27,8 @@ class SwaggerConfigTest {
         var response = mockMvc
                 .perform(post("application/json"))
                 .andReturn().getResponse();
+
+        assertThat(response.getStatus())
+                .isEqualTo(HttpStatus.BAD_REQUEST.value());
     }
 }
